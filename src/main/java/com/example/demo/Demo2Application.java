a610f8b0ba4a90aae60ae112083b9fe7fad2df6c
avirala6 <avirala6@gmail.com>
2021-08-24 23:17:59 +0530

    added a function

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -6,9 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo2Application {
 //jenkins comment	
+	
 	public static void main(String[] args) {
 		System.out.println("welcome!");
+		fun();
 		SpringApplication.run(Demo2Application.class, args);
+	}
+	public static void fun() {
+		System.out.println("this is a function()");
 	}
 
 }
